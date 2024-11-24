4f25ea29911f8d9dc854bd055c07f69058c68d8c
YuryXXl <y7183605555@icloud.com>
2024-11-24 13:44:21 -0600

    Excuse Generator

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,20 @@ window.onload = function () {
     const excuse = document.getElementById('excuse');
 
     // 2. Generate a random excuse
+    const part1 = ['A dragon', 'The cat', 'The dog', 'The unicorn', 'Spider-Man'];
+    const part2 = ['stole', 'misplaced', 'ate', 'rewrote', 'webbed'];
+    const part3 = ['my sword', 'my report', 'my homework', 'my code', 'my laptop'];
+    const part4 = ['last night', 'yesterday', 'this morning', 'this afternoon', 'this evening'];
 
-    const excuseText = `The dog ate my homework for breakfast`;
+    const indexPart1 = Math.floor(Math.random() * part1.length);
+    const indexPart2 = Math.floor(Math.random() * part2.length);
+    const indexPart3 = Math.floor(Math.random() * part3.length);
+    const indexPart4 = Math.floor(Math.random() * part4.length);
+
+    const excuseText = `${part1[indexPart1]} ${part2[indexPart2]} ${part3[indexPart3]} ${part4[indexPart4]}`;
+    
+
+    
 
     // 3. Inseart the excuse into the excuse element
     excuse.innerHTML = excuseText;
